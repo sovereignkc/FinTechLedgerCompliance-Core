7663c785655106cd65d844d4de416ea6e38b02b4
Kevlar Chi <kevlarchi313@gmail.com>
2026-08-29 16:14:14 -0700

    fix: modernize legacy C and COBOL clearing engines and verify test suite

---
+++ backend/build/CMakeFiles/ledger_api.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ledger_api.dir/ledger_api.cpp.o"
+  "CMakeFiles/ledger_api.dir/ledger_api.cpp.o.d"
+  "ledger_api"
+  "ledger_api.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ledger_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
